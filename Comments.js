5756c30f1fb091b2e7736630b9764c38f10f9caa
Juan Romero <slayerforce880@gmail.com>
2023-03-27 09:11:22 -0600

    Exercise-Write your first JavaScript code
    Introduction to Mobile Development and JavaScript.
    Exercise 1: Write your first JavaScript code

---
+++ Comments.js
@@ -0,0 +1,60 @@
+// There are two varieties of comments in JavaScript: 
+
+//Single-line comments 
+
+//Multi-line comments
+
+//A single-line comment is created when you add two forward-slash characters one after the other, without spaces
+
+
+
+
+
+//         ---------                            THIS IS A SINGLE LINE COMMENT          -----------
+
+
+
+// A multi-line comment, as its name says, spans for several lines of code and is created with a forward slash and a star. For example: 
+
+
+/*
+                THIS 
+                IS
+                A
+                MULTI-LINE
+                COMMENT
+
+*/
+
+
+// You can also use the multi-line comment syntax on a single line of code
+
+/*
+
+Why writing comments is empowering
+In this course, it is assumed that you've never written a single line of JavaScript code.
+
+With this assumption in mind, consider the effects of what you've just learned, that is, the effects of learning how to write comments in JavaScript: 
+
+You can now freely express your ideas about any code that you write. 
+
+You can add comments to any code that already exists.
+
+Those comments can be intended for your future self, or for colleagues on your development team.
+
+So, comments are empowering because they facilitate communication with your future self or with your team members, allowing you to ask questions about the code, mark the code as "to do", or as "to improve", or just simply explain what a given piece of code does. 
+
+Additionally, you can even comment out some working code in a JavaScript file - to prevent it from running.
+
+Effectively, comments allow you to "switch off" pieces of JavaScript code.
+
+There can be many reasons for that: 
+
+Trying to understand how a given piece of code works.
+
+Testing different solutions to a coding problem - while not having to delete existing code.
+
+Debugging - trying to pin-point why your code is broken or behaving unprediticably.
+
+*/
+
